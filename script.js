005e235cf3b687af1003bceea0df86901feb0bce
Deepak Gupta <dg8574144545@gmail.com>
2023-10-02 00:40:15 +0530

    /

--- script.js
+++ script.js
@@ -1,17 +1,62 @@
-document.getElementById("registrationForm").addEventListener("submit", function (e) {
+// Create an array to store form submissions
+const submissions = [];
+
+document.getElementById("enrollmentForm").addEventListener("submit", function (e) {
     e.preventDefault();
 
-    const firstName = document.getElementById("firstName").value;
-    const lastName = document.getElementById("lastName").value;
+    const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
+    const portfolio = document.getElementById("portfolio").value;
+    const image = document.getElementById("image").value;
+    const gender = document.querySelector('input[name="gender"]:checked')?.value || 'Not specified';
+    const skills = [...document.querySelectorAll('input[name="skills"]:checked')].map(skill => skill.value);
 
-    // You can perform further validation here if needed
+    // Create an object to represent the submission
+    const submission = {
+        name,
+        email,
+        portfolio,
+        image,
+        gender,
+        skills,
+    };
 
-    const output = document.getElementById("output");
-    output.innerHTML = `
-        <h2>Registration Successful</h2>
-        <p>First Name: ${firstName}</p>
-        <p>Last Name: ${lastName}</p>
-        <p>Email: ${email}</p>
-    `;
+    // Add the submission to the submissions array
+    submissions.push(submission);
+
+    // Clear the form fields
+    document.getElementById("enrollmentForm").reset();
+
+    // Display all submissions
+    displaySubmissions();
 });
+
+document.getElementById("clearButton").addEventListener("click", function () {
+    document.getElementById("enrollmentForm").reset();
+});
+
+// Function to display all submissions
+function displaySubmissions() {
+    const output = document.getElementById("output");
+    output.innerHTML = '';
+
+    submissions.forEach((submission, index) => {
+        const submissionDiv = document.createElement('div');
+        submissionDiv.className = 'submission';
+
+        submissionDiv.innerHTML = `
+        <h2>Submission ${index + 1}</h2>
+        <p>Name: ${submission.name}</p>
+        <p>Email: ${submission.email}</p>
+        <p>Portfolio Website: <a href="${submission.portfolio}" target="_blank">${submission.portfolio}</a></p>
+        <div id="imageContainer">
+            <img src="${submission.image}" alt="Student's Image">
+        </div>
+        <p>Gender: ${submission.gender}</p>
+        <p>Skills: ${submission.skills.join(", ")}</p>
+    `;
+    
+
+        output.appendChild(submissionDiv);
+    });
+}
